d41f84321ff0350bb6af59bf37c3677ff3b16ec7
Ivan Prignano <ivanprignano@gmail.com>
2018-12-28 16:47:47 +0100

    Component folder restructuring, add Logo component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from '../Logo';
+import styles from './styles.module.scss';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <Logo />
+    </div>
+  )
+}
+
+export default App;
